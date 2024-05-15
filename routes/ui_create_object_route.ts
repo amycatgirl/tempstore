f0404b1d849e98ead777686475fd5d61fb0c6d09
Amy <amy@amogus.cloud>
2024-05-15 10:48:33 -0400

    chore: fix import, make ui a bit fancier (no css, won't do)

--- routes/ui_create_object_route.ts
+++ routes/ui_create_object_route.ts
@@ -1,32 +1,44 @@
-import { BadRequest, Created, InternalServerError, NotAcceptable } from "wren/response.ts";
+import {
+  BadRequest,
+  Created,
+  InternalServerError,
+  NotAcceptable,
+} from "wren/response.ts";
 import { POST } from "wren/route.ts";
 import { StorageSingleton } from "../storage.ts";
+import { html } from "html/mod.ts";
 
 const UICreateObjectRoute = POST("/", async (req) => {
   const form = await req.formData();
   const file = form.get("file");
-  const until: unknown = form.get("until");
+  const until: unknown = form.get("until") ?? 1;
   const storage = StorageSingleton.getInstance();
 
   if (!file) {
     return BadRequest();
   } else if (file instanceof File) {
     try {
-      const { id, filename, stored_until } = await storage.storeFile(file, (until as number | undefined) ?? 1);
+      const { id, filename } = await storage.storeFile(
+        file,
+        until as number | undefined,
+      );
 
-    console.log(`[CREATE] File ${filename} was created`)
-    return Created({
-      id,
-      filename,
-      message: `Saved ${filename} until ${new Date(stored_until)}`,
-    });
-    } catch(error) {
-      return InternalServerError({message: "Error whilst creating object", error: error.message})
+      console.log(`[CREATE] File ${filename} was created`);
+      return Created(`
+      Stored file "${filename}". The file will expire ${
+        (until as number) > 1 ? `in ${until}` : "an"
+      } hour${(until as number) > 1 ? "s" : ""}
+The file's id is: ${id}      
+      `.trim());
+    } catch (error) {
+      return InternalServerError({
+        message: "Error whilst creating object",
+        error: error.message,
+      });
     }
   } else {
     return NotAcceptable();
   }
 });
-
 
 export default UICreateObjectRoute;
--- templates/index.ts
+++ templates/index.ts
@@ -2,11 +2,39 @@ import { html } from "html/mod.ts"
 
 export const uploaderTemplate = html`
 	<body>
+		<pre>
+         *                  *
+             __                *
+          ,db'    *     *
+         ,d8/       *        *    *
+         888
+         \`db\       *     *
+           \`o\`_                    **
+      *               *   *    _      *
+            *                 / )
+         *    (\__/) *       ( (  *
+       ,-.,-.,)    (.,-.,-.,-.) ).,-.,-.
+      | @|  ={      }= | @|  / / | @|o |
+     _j__j__j_)     \`-------/ /__j__j__j_
+     ________(               /___________
+      |  | @| \\              || o|O | @|
+      |o |  |,'\\       ,   ,'"|  |  |  |  hjw
+     vV\\|\/vV|\`-'\\  ,---\\   | \\Vv\\hjwVv\\//v
+                _) )    \`. \ /
+               (__/       ) )
+                         (_/
+
+                  tempstore
+		</pre>
+		<br/>
+		<div>
 		<form method="post" enctype="multipart/form-data">
 			<label for="file">Choose a file to upload</label>
 			<input type="file" id="file" name="file">
+			<br/>
 
 			<button>Upload</button>
 		</form>
+		</div>
 	</body>
 `
